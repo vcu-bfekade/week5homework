b69d1b74f403b638c9d6173adade922126d894b0
Bruk Fekade <bfekade@users.noreply.github.com>
2021-03-08 17:53:13 -0500

    Update homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -6,6 +6,8 @@ def find_greatest_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the largest number in the list.
     """
+    print(max(incoming_list))
+
     pass
 
 
@@ -14,6 +16,7 @@ def find_least_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the smallest/least number in the list.
     """
+    print(min(incoming_list))
     pass
 
 
@@ -22,6 +25,7 @@ def add_list_numbers(incoming_list):
     Required parameter, incoming_list, should be a list.
     Add all the values together and return it.
     """
+    print(sum(incoming_list))
 
     pass
 
@@ -31,4 +35,6 @@ def longest_value_key(incoming_dict):
     Required parameter, incoming_dict, should be a dict.
     Find the KEY that has a value with the highest length, use the len() function
     """
+    print(max(len(incoming_dict)))
+
     pass
