4a087fb3edb61fbb304a3e02a2dbf99f09f2a468
Bruk Fekade <bfekade@users.noreply.github.com>
2021-03-08 18:01:33 -0500

    Update homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -8,8 +8,6 @@ def find_greatest_number(incoming_list):
     """
     print(max(incoming_list))
 
-    pass
-
 
 def find_least_number(incoming_list):
     """
@@ -17,7 +15,7 @@ def find_least_number(incoming_list):
     Find the smallest/least number in the list.
     """
     print(min(incoming_list))
-    pass
+    
 
 
 def add_list_numbers(incoming_list):
@@ -27,7 +25,6 @@ def add_list_numbers(incoming_list):
     """
     print(sum(incoming_list))
 
-    pass
 
 
 def longest_value_key(incoming_dict):
@@ -37,4 +34,3 @@ def longest_value_key(incoming_dict):
     """
     print(max(len(incoming_dict)))
 
-    pass
