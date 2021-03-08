1a5d32a9614553c423525f3a6ec1fe724e401a81
Bruk Fekade <bfekade@users.noreply.github.com>
2021-03-08 17:35:58 -0500

    Update test_homework.py

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -27,8 +27,7 @@ def test_key_with_the_longest_value():
     """A little trickier.   Which KEY has the 'longest' value associated with it? """
 
     assert (
-        homework.longest_value_key(
-            {"dog": "cat", "a": "asdfasdfasdfasdfasdf"}) == "a"
+        homework.longest_value_key({"dog": "cat", "a": "asdfasdfasdfasdfasdf"}) == "a"
     )
     assert homework.longest_value_key({}) is None
     assert homework.longest_value_key(None) is None
