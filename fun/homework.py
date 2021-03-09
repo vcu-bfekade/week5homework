e27ecb09abf147029026b95d09a31aa0f7efb81b
Bruk Fekade <bfekade@users.noreply.github.com>
2021-03-08 20:59:09 -0500

    Revert "Update homework.py"
    This reverts commit 079466c702ba0ace5804030a2c0688bcd55fb119.

--- fun/homework.py
+++ fun/homework.py
@@ -6,32 +6,29 @@ def find_greatest_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the largest number in the list.
     """
-
     maxiumum = max(incoming_list)
     return maxiumum
-
+   
 
 def find_least_number(incoming_list):
     """
     Required parameter, incoming_list, should be a list.
     Find the smallest/least number in the list.
     """
-
     least = min(incoming_list)
     return least
-
 
 def add_list_numbers(incoming_list):
     """
     Required parameter, incoming_list, should be a list.
     Add all the values together and return it.
     """
-
     if incoming_list is None or len(incoming_list) == 0:
         list_sum = None
     else:
         list_sum = sum(incoming_list)
     return list_sum
+    
 
 
 def longest_value_key(incoming_dict):
@@ -39,8 +36,7 @@ def longest_value_key(incoming_dict):
     Required parameter, incoming_dict, should be a dict.
     Find the KEY that has a value with the highest length, use the len() function
     """
-
     d = incoming_dict
     d = None
-    biggest = max(d, key=len)
+    biggest = max(d, key = len)
     return biggest
